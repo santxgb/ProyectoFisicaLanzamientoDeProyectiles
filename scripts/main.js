81b9be8e6ac3b67dcb32711835ed52a877c754be
santxgb <gabrielsantiagobordacurrea@gmail.com>
2026-04-16 22:03:16 -0500

    Se conectan las variables creadas dentro del HTML, con la clase de main de JavaScript

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+const canvas = document.getElementById('canvas');
+
+const slAngulo = document.getElementById('sl-angulo');
+const slVelocidad = document.getElementById('sl-velocidad');
+const slAltura = document.getElementById('sl-altura');
+const slGravedad = document.getElementById('sl-gravedad');
+
+const valAngulo = document.getElementById('val-angulo');
+const valVelocidad = document.getElementById('val-velocidad');
+const valAltura = document.getElementById('val-altura');
+const valGravedad = document.getElementById('val-gravedad');
+
+const btnLanzar = document.getElementById('btn-lanzar');
+const btnReset = document.getElementById('btn-reset');
+
+const dTiempo = document.getElementById('d-tiempo');
+const dX = document.getElementById('d-x');
+const dY = document.getElementById('d-y');
