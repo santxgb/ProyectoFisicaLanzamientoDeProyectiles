3639e49acc402957dac4d201d660873845a36cc7
santxgb <gabrielsantiagobordacurrea@gmail.com>
2026-04-26 12:30:04 -0500

    Se terminan de agregar las funciones base del modelo físico. Siguiente paso hacer pruebas manuales

--- scripts/physics.js
+++ scripts/physics.js
@@ -30,3 +30,49 @@ function calcularVelocidadY(velocidadInicial, anguloGrados, gravedad, tiempo){
     const { vy } = calcularComponentesVelocidad(velocidadInicial, anguloGrados);
     return vy - gravedad * tiempo;
 }
+
+function calcularMagnitudVelocidad(vx, vy){
+    return Math.sqrt(vx * vx + vy * vy);
+}
+
+function calcularAlturaMaxima(velocidadInicial, anguloGrados, alturaInicial, gravedad){
+    const { vy } = calcularComponentesVelocidad(velocidadInicial, anguloGrados);
+    return alturaInicial + (vy * vy) / (2 * gravedad);
+}
+
+function calcularTiempoVuelo(velocidadInicial, anguloGrados, alturaInicial, gravedad){
+    const { vy } = calcularComponentesVelocidad(velocidadInicial, anguloGrados);
+
+    const discriminante = vy * vy + 2 * gravedad * alturaInicial;
+
+    if(discriminante < 0){
+        return 0;
+    }
+
+    return(vy + Math.sqrt(discriminante)) / gravedad;
+}
+
+function calcularAlcanceHorizontal(velocidadInicial, anguloGrados, alturaInicial, gravedad){
+    const tiempoVuelo = calcularTiempoVuelo(velocidadInicial, anguloGrados, alturaInicial, gravedad);
+
+    return calcularPosicionX(velocidadInicial, anguloGrados, tiempoVuelo);
+}
+
+function calcularEstadoProyectil(parametros, tiempo){
+    const x = calcularPosicionX(parametros.velocidadInicial, parametros.angulo, tiempo);
+    const y = calcularPosicionY(parametros.velocidadInicial, parametros.angulo, parametros.alturaInicial, parametros.gravedad, tiempo);
+
+    const vx = calcularVelocidadX(parametros.velocidadInicial, parametros.angulo);
+    const vy = calcularVelocidadY(parametros.velocidadInicial, parametros.angulo, parametros.gravedad, tiempo);
+
+    const velocidad = calcularMagnitudVelocidad(vx, vy);
+
+    return {
+        tiempo, 
+        x, 
+        y,
+        vx,
+        vy,
+        velocidad
+    };
+}
